bc46576fc3ee46180baef6b3630a14bb346b542f
Magda B <borkmag@gmail.com>
2020-03-29 14:12:34 +0200

    Changes in tests

--- conftest.py
+++ conftest.py
@@ -1,25 +1,26 @@
-
 import pytest
+from allure_commons.types import AttachmentType
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-from pages.inventory import InventoryPage
-from pages.login import LoginPage
+from pages.inventory_page import InventoryPage
+from pages.login_page import LoginPage
+from pages.cart_page import CartPage
+import allure
 
 
-
-# @pytest.fixture()
-# def setup(request):
-#     driver = webdriver.Chrome(ChromeDriverManager().install())
-#     driver.get('https://www.saucedemo.com/')
-#     driver.implicitly_wait(10)
-#     driver.maximize_window()
-#     request.cls.driver = driver
-#     request.cls.login_page = LoginPage(driver)
-#     before_failed = request.session.testsfailed
-#     yield
-#     # if request.session.testsfailed != before_failed:
-#     #     allure.attach(driver.get_screenshot_as_png(), name='Test failed', attachment_type=AttachmentType.PNG)
-#     driver.quit()
+@pytest.fixture()
+def setup(request):
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver.get('https://www.saucedemo.com/')
+    driver.implicitly_wait(10)
+    driver.maximize_window()
+    request.cls.driver = driver
+    request.cls.login_page = LoginPage(driver)
+    before_failed = request.session.testsfailed
+    yield
+    if request.session.testsfailed != before_failed:
+        allure.attach(driver.get_screenshot_as_png(), name='Test failed', attachment_type=AttachmentType.PNG)
+    driver.quit()
 
 
 @pytest.fixture()
@@ -30,7 +31,11 @@ def setup1(request):
     driver.maximize_window()
     request.cls.driver = driver
     request.cls.inventory_page = InventoryPage(driver)
+    request.cls.cart_page = CartPage(driver)
+    before_failed = request.session.testsfailed
     yield
+    if request.session.testsfailed != before_failed:
+        allure.attach(driver.get_screenshot_as_png(), name='Test failed', attachment_type=AttachmentType.PNG)
     driver.quit()
 
 
--- tests/test_cart.py
+++ tests/test_cart.py
@@ -3,13 +3,13 @@ import allure
 
 
 @pytest.mark.usefixtures('setup1')
-class TestCart:
+class TestInventory:
 
     @allure.step('Test cart quantity')
     def test_cart_quantity(self):
         for n in range(1, 7):
             self.inventory_page.click_on_element(n)
-        assert self.inventory_page.get_cart_quantity() == '6'
+        assert self.cart_page.get_cart_quantity() == '6'
 
     @allure.step('Test remove btn')
     def test_remove_btn(self):
@@ -21,24 +21,22 @@ class TestCart:
         for n in range(1, 3):
             self.inventory_page.click_on_element(n)
         self.inventory_page.click_on_element(1)
-        assert self.inventory_page.get_cart_quantity() == '1'
+        assert self.cart_page.get_cart_quantity() == '1'
 
     @allure.step('Test continue shopping')
     def test_continue_shopping(self):
         self.inventory_page.click_on_element(1)
-        assert self.inventory_page.get_cart_quantity() == '1'
+        assert self.cart_page.get_cart_quantity() == '1'
 
         self.inventory_page.click_on_cart_icon()
-        assert self.inventory_page.get_cart_header() == 'Your Cart'
+        assert self.cart_page.get_cart_header() == 'Your Cart'
 
         self.inventory_page.click_on_continue_shopping()
         assert self.inventory_page.get_products_header() == 'Products'
 
     @allure.step('Test products name')
-    def test_produscts_name_cart(self):
+    def test_products_name_cart(self):
         for n in range(1, 7):
             self.inventory_page.click_on_element(n)
         self.inventory_page.click_on_cart_icon()
-        assert self.inventory_page.get_cart_element_name(3) == 'Sauce Labs Backpack'
-
-#  nad asercjami opis czego dotyczą, pod testem jaki jest oczekiwany rezultat - komentarze
+        assert self.cart_page.get_cart_element_name(3) == 'Sauce Labs Backpack'
