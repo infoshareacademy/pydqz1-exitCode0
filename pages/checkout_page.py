98cf1944407373fa04123c580afee88b1b96126d
Konrad Linka <konrad.linka@gmail.com>
2020-04-03 19:44:16 +0200

    test ok

--- pages/checkout_page.py
+++ pages/checkout_page.py
@@ -12,8 +12,8 @@ class CheckoutPage:
         self.last_name_input = '#last-name'
         self.postal_code_input = '#postal-code'
         self.btn_continue = '.btn_primary'
-        self.btn_cancel = '.#checkout_info_container > div > form > div.checkout_buttons > a'
-        self.cancel_cart = (By.CSS_SELECTOR, '#contents_wrapper > div.subheader')
+        self.btn_cancel = '//*[@id="checkout_info_container"]/div/form/div[2]/a'
+        self.cancel_cart = (By.CSS_SELECTOR, '.subheader')
 
     def person_data(self):
         self.driver.find_element_by_css_selector(self.checkout_btn).click()
@@ -23,8 +23,9 @@ class CheckoutPage:
         self.driver.find_element_by_css_selector(self.btn_continue).click()
 
     def cancel_person_data(self):
-        self.driver.find_element_by_css_selector(self.btn_cancel).click()
+        self.driver.find_element_by_css_selector(self.checkout_btn).click()
+        self.driver.find_element_by_xpath(self.btn_cancel).click()
 
-    def cancel_cart(self):
+    def cancel_cart_finder(self):
         cart_header = self.driver.find_element(*self.cancel_cart)
         return cart_header.text
--- tests/test_checkout_feature.py
+++ tests/test_checkout_feature.py
@@ -5,26 +5,26 @@ import allure
 @pytest.mark.usefixtures('setup3')
 class TestCheckoutFeature:
 
-    # @allure.step('Test checkout feature continue')
+    @allure.step('Test checkout feature continue')
     def test_checkout_feature_continue(self):
         self.checkout_page.person_data()
         assert self.cart_page.get_cart_header() == "Checkout: Overview"
 
-    # @allure.step('Test checkout feature cancel')
+    @allure.step('Test checkout feature cancel')
     def test_checkout_feature_cancel(self):
         self.checkout_page.cancel_person_data()
-        assert self.cart_page.get_cart_header() == "Your Cart"
+        assert self.checkout_page.cancel_cart_finder() == "Your Cart"
 
 
 @pytest.mark.usefixtures('setup4')
 class TestOverwievFeature:
 
-    # @allure.step('Test checkout overwiev finish')
+    @allure.step('Test checkout overwiev finish')
     def test_checkout_overwiev_finish(self):
         self.checkout_overview_page.finish()
         assert self.checkout_overview_page.finish_header_finder() == "Finish"
 
-    # @allure.step('Test checkout overwiev cancel')
+    @allure.step('Test checkout overwiev cancel')
     def test_checkout_overview_cancel(self):
         self.checkout_overview_page.cancel()
         assert self.checkout_overview_page.product_header_finder() == "Products"
